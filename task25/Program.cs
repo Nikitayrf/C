a7edeab608af8724597c029bf6bac7aea630f1f2
Nikitayrf <samaraphone@yandex.ru>
2022-11-23 19:40:08 +0100

    add task 25

---
+++ task25/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную
+степень B.*/
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введитите число A : ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введитите число B : ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int sum = 1;
+for (int i = 1; i <= b;i++)
+{
+    sum = sum * a;
+}
+Console.WriteLine(sum);
