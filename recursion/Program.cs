1031680d73c2f6046362eb23fc0563e98716bec7
Nikitayrf <samaraphone@yandex.ru>
2022-12-02 10:17:11 +0100

    add recursion

---
+++ recursion/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу для подсчета количества цифр в числе с помощью рекурсии.
+
+12345 -> 5 */
+
+Console.WriteLine("Ведите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+void recursion(int num, int count)
+{
+    if (num == 0)
+    {
+        Console.WriteLine($"Количество цифр в числе равно : {count}");
+        return;
+    }
+    count++;
+    recursion(num / 10, count);
+}
+
+recursion(num, count);
